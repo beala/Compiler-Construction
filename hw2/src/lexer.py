7b6de8295bab24ae25a5b575fdfc8f467f0b4395
Josh Wepman <josh.wepman@gmail.com>
2011-09-07 13:12:24 -0600

    Combined into one file.

--- hw2/src/lexer.py
+++ hw2/src/lexer.py
@@ -1,6 +1,7 @@
 class Lexer:
 	lexer = None
-	tokens = None
+	parser = None
+	
 	def __init__(self):
 		reserved = {'print' : 'PRINT'}
 		
@@ -46,12 +47,57 @@ class Lexer:
 		import ply.lex as lex
 		self.lexer = lex.lex()
 		
+		#------------- PARSER
+		
+		from compiler.ast import Printnl, Add, Const, UnarySub, CallFunc, Assign, AssName
+		lex = lexer.Lexer()
+		tokens = lex.getTokens()
+		#define precedence
+		precedence = (
+			('right','ASSIGN'),
+			('left','PLUS'),
+			('right', 'NEGATE'),
+			('left','L_PAREN','R_PAREN')
+		)
+		
+		#define parse logic
+		def p_print_statement(t):
+			'statement : PRINT expression'
+			t[0] = Printnl([t[2]], None)
+		def p_assign_statement(t):
+			'statement : NAME = expression'
+			t[0] = Assign(AssName(t[1],'OP_ASSIGN'),t[3])
+		
+		def p_plus_expression(t):
+			'expression : expression PLUS expression'
+			t[0] = Add((t[1], t[3]))
+		def p_negate_expression(t):
+			'expression : NEGATE expression'
+			t[0] = UnarySub(t[2])
+		def p_int_expression(t):
+			'expression : INT'
+			t[0] = Const(t[1])
+		def p_name_expression(t):
+			'expression : NAME'
+			t[0] = Name(t[1])
+		def p_func_expression(t):
+			'expression : FUNC'
+			t[0] = CallFunc(t[1],None,None,None)
+		def p_l_paren_expression_r_paren(t):
+			'expression : L_PAREN expression R_PAREN'
+			t[0] = t[2]
+		
+		def p_error(t):
+			print "Syntax Error at '%s'" % t.value
+		
+		import ply.yacc as yacc
+		self.parser = yacc.yacc()
+
+		
 	def test_lex(self, to_lex):
 		self.lexer.input(to_lex)
 		while True:
 			tok = self.lexer.token()
 			if not tok: break
 			print tok
-			
-	def getTokens(self):
-		return self.tokens		
+		

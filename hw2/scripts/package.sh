bb7a0fc12c5a65b2aeba7ceb1aec79259bc8173f
Alex Beal <alexlbeal@gmail.com>
2011-09-08 16:00:27 -0600

    1) Made a package script that puts a zip of the src dir in the build dir. 2) added .gitignore to the build dir so it sync with github. 3) As is, the code passes all the grader tests!

---
+++ hw2/scripts/package.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_PATH="`dirname $0`"
+TESTS_DIR="$SCRIPT_PATH/../tests"
+BUILD_DIR="$SCRIPT_PATH/../build"
+SRC_DIR="$SCRIPT_PATH/../src"
+
+cd $SRC_DIR
+zip -r $BUILD_DIR/hw2.zip *

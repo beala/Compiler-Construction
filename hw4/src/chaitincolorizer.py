e3974e094d7b6a0b68ee4475a33b6d6d73e7f8c3
Josh Wepman <josh.wepman@gmail.com>
2011-10-19 12:59:44 -0600

    Chaitin colorizer with weight for unspillables

--- hw4/src/chaitincolorizer.py
+++ hw4/src/chaitincolorizer.py
@@ -2,21 +2,29 @@
 #Written by Josh Wepman, joshua.wepman@colorado.edu
 from heappriorityqueue import * #we use a heapq to store sorted list of nodes by degree < k
 import copy
+from x86 import *
 class ChaitinColorizer(object):
 	theGraph = {} #nodes to list of edges
 	heapQueue = None #heapified PQ of nodes by degree
 	colorMap = {} #list of colors (registers input, addt'l colors added as necessary color => register name or stack location
 	stackOffset = 4
 	def __init__(self, graph, colors={1:'eax',2:'ebx',3:'ecx',4:'edx'}):
-		self.theGraph = graph
+		self.theGraph = copy.copy(graph)
 		self.copyGraph = copy.copy(graph)
 		self.colorMap = colors
-		self.heapQueue = HeapPriorityQueue()
-		for node in self.theGraph:
-			self.heapQueue.add_task(len(self.theGraph[node]),node)
+		self.heapQueue = HeapPriorityQueue(True)
+		for node in self.theGraph.keys():
+			if not isinstance(node, VarNode) or node.color != -1:
+				continue
+			spillFactor = 0
+			if node.spillable == False:
+				spillFactor = -100
+			self.heapQueue.add_task(len(self.theGraph[node])+spillFactor,node)
 	def doColor(self):
 		curNode = None
 		myColorStack = []
+		mySpillStack = []
+			
 		while not self.heapQueue.empty():
 			curNode = self.heapQueue.pop_task() 
 			myLen = 0
@@ -25,31 +33,42 @@ class ChaitinColorizer(object):
 			except KeyError:
 				pass
 			if myLen > len(self.colorMap):
-				#spill the graph :(
-				curNode.color = self.addNewColor()
-			else:
 				myColorStack.append(curNode)
+			else:
+				mySpillStack.append(curNode)
 			self.doRemoveNodeConnections(curNode)
 		#pop stack and colorize!
 		while len(myColorStack) > 0:
 			curNode = myColorStack.pop()
-			curNode.color = self.doCalculateLowestAvailColor(curNode)
+			try:
+				curNode.color = self.doCalculateLowestAvailColor(curNode)
+			except ValueError:
+				mySpillStack.insert(0,curNode)
+		while len(mySpillStack) > 0:
+			curNode = mySpillStack.pop()
+			try:
+				curNode.color = self.doCalculateLowestAvailColor(curNode)
+			except ValueError:
+				#spill forced
+				curNode.color = self.addNewColor()
 		#at this point, all nodes have been colored!
 		return self.colorMap
 	def doCalculateLowestAvailColor(self,node):
+		if len(self.copyGraph[node]) == 0:
+			return 1
 		adjacentColors = set([])
 		try:
 			for myNeighbor in self.copyGraph[node]:
+				if myNeighbor.color == -1:
+					continue
 				adjacentColors.add(myNeighbor.color)
-			#return set(self.colorMap.keys()) - adjacentColors
-			adjacentColors = set(adjacentColors)
 			for element in sorted(self.colorMap.keys()):
 				if element not in adjacentColors:
 					return element
 		except KeyError:
 			#first color works :)
-			return 1
-		raise Exception("No suitable color found :(")
+			raise KeyError("Some weird key error trying to find lowest avail color")
+		raise ValueError("No suitable color found :(")
 	def addNewColor(self):
 		newColor = (4 + (self.stackOffset/4)) 
 		self.colorMap[newColor] = self.stackOffset
@@ -58,13 +77,14 @@ class ChaitinColorizer(object):
 	def doRemoveNodeConnections(self,node):
 		#remove a node and all of its connections from the graph
 		try:
-			connectedList = copy.copy(self.theGraph[node])
+			connectedList = copy.copy(self.copyGraph[node])
 			for connectedElement in connectedList:
 				try:
 					self.theGraph[connectedElement].remove(node)
 					self.heapQueue.add_task(len(self.theGraph[connectedElement]), connectedElement) #update the heapqueue with new degree
 				except ValueError:
-					continue #somehow not in graph :S
+					raise ValueError("Not in graph...hmmmm")
+					#continue #somehow not in graph :S
 			del self.theGraph[node]
 		except KeyError:
 			return #already not in graph :)

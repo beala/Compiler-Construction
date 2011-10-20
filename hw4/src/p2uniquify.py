63768c753a8ed5a3d195708ed73e4e15d271f058
Pair Programming <alexlbeal@gmail.com>
2011-10-19 20:30:17 -0600

    Wrote new getLocals function. Just decided that we can't change the AST nodes while uniquifying. That will have to be it's own step, so I'm about to remove the code that does that.

--- hw4/src/p2uniquify.py
+++ hw4/src/p2uniquify.py
@@ -1,15 +1,51 @@
-from astvisitor import *
+from compiler import *
+from compiler.ast import *
+
+class ASTVisitor(object):
+	def visit(self, node, curScopeDict={}):
+		'''Visit a node'''
+		# Find a specific visit method. Default to "default".
+		methname = "visit_%s" % node.__class__.__name__
+		method = getattr(self, methname, self.default)
+		return method(node, curScopeDict)
+    
+	def default(self, node):
+		'''Visit node children'''
+		print "Uh oh: A method doesn't exist for this node type: %s" % node.__class__.__name__
 
 class P2Uniquify(ASTVisitor):
 	globalCounter = 0
 	# Private Functions: ######################################################################################
 
+	def _getLocals(self, node):
+		local_vars = []
+		if isinstance(node, Assign):
+			local_vars += [node.nodes[0].name]
+			return local_vars
+		elif isinstance(node, Function):
+			local_vars += [node.name]
+			local_vars += stmt.argnames
+			for stmt in node.code
+				local_vars += self._getLocals(stmt)
+			return local_vars
+		elif isinstance(node, Lambda):
+			local_vars += node.argnames
+			for stmt in node.code
+				local_vars += self._getLocals(stmt)
+			return local_vars
+		elif isinstance(node, Module):
+			for stmt in node.node.nodes
+				local_vars += self.getLocals(stmt)
+			return local_vars
+		else
+			raise TypeError("Uh oh! What's a " + str(node.__class__.__name___) + "????")
+				
 	# Desc:	Returns a list of variables that are local to the scope it's given.
 	# Args:	stmt_list: A list of AST nodes that represents an arbitrary scope.
 	#			This is usually from the 'code' attribute within a Lambda
 	#			or Function node.
 	# Ret:	A set of variable names that are local to the given scope.
-	def _getLocals(self,stmt_node):
+	def _getLocalsOLD(self,stmt_node):
 		#get anything that is assigned-to
 		#assume ast is a statement node (list of statements)
 		# List which will hold the names of all local variables.
@@ -17,16 +53,17 @@ class P2Uniquify(ASTVisitor):
 
 		for stmt in stmt_node.nodes:
 			# If it's on the LHS of an assignment, then it's local to this scope.
-			if isinstance(stmt, Assign)
+			if isinstance(stmt, Assign):
 				local_vars += [stmt.nodes[0].name]
 			# Recurse into new scopes (functions and lambdas) and get their local vars,
 			# and add to the current list of local vars.
 			elif isinstance(stmt, Function):
-				local_vars += [stmt.argnames] # Args count as an assignment
+				local_vars += [stmt.name] # For uniquifying function names
+				local_vars += [element.name for element in stmt.argnames] # Args count as an assignment
 				local_vars += self._getLocals(stmt.code)
 			elif isinstance(stmt, Lambda):
-				local_vars += [stmt.argnames] # Args count as an assignment
-				local_vars += self._getLocals([stmt.code]) # Lambdas only have one stmt, not a stmt_list
+				local_vars += [element.name for element in stmt.argnames] # Args count as an assignment
+				local_vars += self._getLocals(stmt.code) # Lambdas only have one stmt, not a stmt_list
 			# Although it's technically not a new scope, we must recurse into IfExp
 			# to see if any assignments occur inside.
 			elif isinstance(stmt, IfExp):
@@ -40,76 +77,108 @@ class P2Uniquify(ASTVisitor):
 		nameNode.name = curScopeDict[nameNode.name]
 	def uniquifyLocalNames(self, localList, curScopeDict):
 		for var in localList:
-			curScopeDict[var] = "{"+var+self.globalCounter
+			curScopeDict[var] = "{"+var+str(self.globalCounter)
 			self.globalCounter += 1
 	# Visitor Functions: #######################################################################################
 
 	def visit_Lambda(self, ast, curScopeDict):
-		localVars = self._getLocals(ast)
+		# Convert all Lambdas to contain a Stmt node in the 'code' attribute to mirror
+		# Function nodes.
+		newRet = Return(ast.code)
+		ast = Lambda([Name(name) for name in ast.argnames] , ast.defaults, ast.flags, Stmt([newRet]))
+		localVars = self._getLocals(Stmt([ast]))
 		self.uniquifyLocalNames(localVars, curScopeDict)
-		self.visit(ast.code, curScopeDict)
+		ast.code = self.visit(ast.code, curScopeDict)
+		for argname in ast.argnames:
+			argname = self.visit(argname, curScopeDict)
+		return ast
 	def visit_Function(self, ast, curScopeDict):
-		localVars = self._getLocals(ast)
+		ast = Function(ast.decorators, Name(ast.name), [Name(name) for name in ast.argnames], ast.defaults, ast.flags, ast.doc, ast.code) 
+		localVars = self._getLocals(Stmt([ast]))
 		self.uniquifyLocalNames(localVars, curScopeDict)
-		self.visit(ast.code, curScopeDict)
+		ast.code = self.visit(ast.code, curScopeDict)
+		for argname in ast.argnames:
+			argname = self.visit(argname, curScopeDict)
+		ast.name = self.visit(ast.name, curScopeDict)
+		return ast
 	def visit_Module(self, ast, curScopeDict={}):
 		localVars = self._getLocals(ast.node)
 		self.uniquifyLocalNames(localVars, curScopeDict)
-		self.visit(ast.node, curScopeDict)
+		ast.node = self.visit(ast.node, curScopeDict)
+		return ast
 	def visit_Return(self, ast, curScopeDict):
-		self.visit(ast.value, curScopeDict)
+		ast.value = self.visit(ast.value, curScopeDict)
+		return ast
 	def visit_Stmt(self, ast, curScopeDict):
 		for node in ast.nodes:
-			self.visit(ast.node, curScopeDict)
+			node = self.visit(node, curScopeDict)
+		return ast
 	def visit_Name(self, ast, curScopeDict):
-		self.rename(curScopeDict, ast)
+		if not (ast.name == 'True' or ast.name == 'False'):
+			self.rename(curScopeDict, ast)
+		return ast
 	def visit_AssName(self, ast, curScopeDict):
 		self.rename(curScopeDict, ast)
+		return ast
 	
-	def visit_Const(self,ast, curScopeDict):
-		pass
+	def visit_Const(self, ast, curScopeDict):
+		return ast
 	def visit_IfExp(self, ast, curScopeDict):
-		self.visit(ast.test, curScopeDict)
-		self.visit(ast.then, curScopeDict)
-		self.visit(ast.else_, curScopeDict)
+		ast.test = self.visit(ast.test, curScopeDict)
+		ast.then = self.visit(ast.then, curScopeDict)
+		ast.else_ = self.visit(ast.else_, curScopeDict)
+		return ast
 	def visit_UnarySub(self, ast, curScopeDict):
-		self.visit(ast.expr, curScopeDict)
+		ast.expr = self.visit(ast.expr, curScopeDict)
+		return ast
 	def visit_Add(self, ast, curScopeDict):
-		self.visit(ast.left, curScopeDict)
-		self.visit(ast.right, curScopeDict)
+		ast.left = self.visit(ast.left, curScopeDict)
+		ast.right = self.visit(ast.right, curScopeDict)
+		return ast
 	def visit_Discard(self, ast, curScopeDict):
-		self.visit(ast.expr, curScopeDict)
+		ast.expr = self.visit(ast.expr, curScopeDict)
+		return ast
 	def visit_Printnl(self, ast, curScopeDict):
-		self.visit(ast.nodes[0], curScopeDict)
+		ast.nodes[0] = self.visit(ast.nodes[0], curScopeDict)
+		return ast
 	def visit_And(self, ast, curScopeDict):
 		for operand in ast.nodes:
-			self.visit(operand, curScopeDict)
+			operand = self.visit(operand, curScopeDict)
+		return operand
 	def visit_Or(self, ast, curScopeDict):
 		for operand in ast.nodes:
-			self.visit(operand, curScopeDict)
+			operand = self.visit(operand, curScopeDict)
+		return ast
 	def visit_Not(self, ast, curScopeDict):
-		self.visit(ast.expr, curScopeDict)
+		ast.expr = self.visit(ast.expr, curScopeDict)
+		return ast
 	def visit_List(self, ast, curScopeDict):
 		for element in ast.nodes:
-			self.visit(element, curScopeDict)
+			element = self.visit(element, curScopeDict)
+		return ast
 	def visit_Dict(self, ast, curScopeDict):
 		for element in ast.items:
-			self.visit(element[0], curScopeDict)
-			self.visit(element[1], curScopeDict)
+			element[0] = self.visit(element[0], curScopeDict)
+			element[1] = self.visit(element[1], curScopeDict)
+		return ast
 	def visit_Subscript(self, ast, curScopeDict):
-		self.visit(ast.expr, curScopeDict)
-		for sub in ast.subs
-			self.visit(sub, curScopeDict)
+		ast.expr = self.visit(ast.expr, curScopeDict)
+		for sub in ast.subs:
+			sub = self.visit(sub, curScopeDict)
+		return ast
 	def visit_Compare(self, ast, curScopeDict):
-		self.visit(ast.expr, curScopeDict)
-		self.visit(ast.ops[1], curScopeDict)
+		ast.expr = self.visit(ast.expr, curScopeDict)
+		ast.ops[1] = self.visit(ast.ops[1], curScopeDict)
+		return ast
 	def visit_CallFunc(self, ast, curScopeDict):
-		self.visit(ast.node, curScopeDict)
+		ast.node = self.visit(ast.node, curScopeDict)
 		for arg in ast.args:
-			self.visit(arg, curScopeDict)
+			arg = self.visit(arg, curScopeDict)
+		return ast
 	def visit_Assign(self, ast, curScopeDict):
-		self.visit(ast.nodes[0], curScopeDict)
-		self.visit(ast.expr, curScopeDict)
+		ast.nodes[0] = self.visit(ast.nodes[0], curScopeDict)
+		ast.expr = self.visit(ast.expr, curScopeDict)
+		return ast
 
 	# Debugging methods: #############################################################################
 	def print_ast(self, stmt_ast, tabcount):
@@ -121,12 +190,12 @@ class P2Uniquify(ASTVisitor):
 				self.print_ast(node.else_, tabcount+1)
 				print '\t' * (tabcount) + 'End If'
 			if isinstance(node, Lambda):
-				print \t * tabcount + 'Lambda (' + str(node.argnames) + '):'
-				self.print_ast(Stmt([node.code]))
+				print '\t' * tabcount + 'Lambda (' + str(node.argnames) + '):'
+				self.print_ast(Stmt([node.code]), tabcount+1)
 				print '\t' * tabcount + 'EndLambda'
 			if isinstance(node, Function):
-				print \t * tabcount + 'def ' + str(node.name) + '(' + str(node.argnames) + '):'
-				self.print_ast(Stmt([node.code]))
+				print '\t' * tabcount + 'def ' + str(node.name) + '(' + str(node.argnames) + '):'
+				self.print_ast(Stmt([node.code]), tabcount+1)
 				print '\t' * tabcount + 'EndFunc'
 			else:
 				print '\t' * (tabcount) + str(node)
@@ -139,9 +208,11 @@ if __name__ == "__main__":
 	import p1flattener
 	if os.path.isfile(sys.argv[1]):
 		print str(compiler.parseFile(sys.argv[1])) + '\n'
-		unique_ast = P2Uniquify().visit(compiler.parseFile(sys.argv[1]))
+		ast = compiler.parseFile(sys.argv[1])
+		P2Uniquify().visit(ast)
 	else:
 		print str(compiler.parse(sys.argv[1])) + '\n'
-		unique_ast = P2Uniquify().visit(compiler.parse(sys.argv[1]))
+		ast = compiler.parse(sys.argv[1])
+		P2Uniquify().visit(ast)
 	print '-' * 10 + 'Uniquified AST' + '-' * 10
-	p1flattener.P1ASTFlattener().print_ast(unique_ast.node, 0)
+	p1flattener.P1ASTFlattener().print_ast(ast.node, 0)

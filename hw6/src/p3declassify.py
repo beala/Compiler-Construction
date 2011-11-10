2cf106509c0d08c85de6ea440f1ef14c2eda746e
Pair Programming <alexlbeal@gmail.com>
2011-11-10 10:23:18 -0700

    Added getattr and assign to declassify. Also fixed some bugs.

--- hw6/src/p3declassify.py
+++ hw6/src/p3declassify.py
@@ -16,12 +16,34 @@ class P3Declassify(ASTVisitor):
 
 	# Visitor Methods: #########################################################################################
 	visit_Class(self, ast):
+		# Make a class, and assign it to a tmpVar
 		classTmp = self._makeTmpVar()
 		tmpAssign = self._makeAssign(classTmp, CreateClass(ast.bases))
-		tmpBody = ast.code.nodes
+		# Declassify the body
+		tmpBody = self.visit(ast.code)
+		# Assign the tmpVar to the class's real name
 		classAssign = self._makeAssign(ast.name, classTmp)
-		return [tmpAssign] + tmpBody + [classAssign]
-	
+		# Return class creation + the body + the class assignment
+		return [tmpAssign] + tmpBody.nodes + [classAssign]
+
+	visit_Assign(self, ast):
+		newAssignExpr = self.visit(ast.expr)
+		# If it's an AssAttr, then call the correct runtime func (SetAttr)
+		if isinstance(ast.nodes[1], AssAttr):
+			assAttrNode = ast.nodes[1]
+			# Declassify the exprs
+			newAssAttrExpr = self.visit(assAttrNode.expr)
+			# Return the new SetAttr node to replace the assign node.
+			return SetAttr(newAssAttrExpr, assAttrNode.attrname, newAssignExpr)
+		else:
+			# Do ast.nodes need to be declassified? I don't think so.
+			return Assign(ast.nodes, newAssignExpr)
+
+	visit_Getattr(self, ast):
+		newGetAttrExpr = self.visit(ast.expr)
+		return GetAttrRuntime(newGetAttrExpr, ast.attrname)
+
+
 	visit_Module(self, ast):
 		return self.visit(ast.node)
 

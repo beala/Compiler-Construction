84cbbda96d7900a1848c69a32037b97d833511ca
Pair Programming <alexlbeal@gmail.com>
2011-11-11 10:22:45 -0700

    Refactored astvisitor to be more flexible. Now uniquify doesn't need its own versin of astvisitor.

--- hw6/src/astvisitor.py
+++ hw6/src/astvisitor.py
@@ -1,12 +1,12 @@
 class ASTVisitor(object):
-	def visit(self, node):
+	def visit(self, node, *extra):
 		'''Visit a node'''
 		# Find a specific visit method. Default to "default".
 		methname = "visit_%s" % node.__class__.__name__
 		method = getattr(self, methname, self.default)
-		return method(node)
+		return method(node, *extra)
 	
-	def default(self, node):
+	def default(self, node, *extra):
 		'''Visit node children'''
-		print "Uh oh: A method doesn't exist for this node type: %s" % node.__class__.__name__
+		raise TypeError("Uh oh: A method doesn't exist for this node type: %s" % node.__class__.__name__)
 		import pdb; pdb.set_trace()
--- hw6/src/p2uniquify.py
+++ hw6/src/p2uniquify.py
@@ -2,18 +2,7 @@ from compiler import *
 from compiler.ast import *
 import copy
 from p2getlocals import *
-
-class ASTVisitor(object):
-	def visit(self, node, curScopeDict={}):
-		'''Visit a node'''
-		# Find a specific visit method. Default to "default".
-		methname = "visit_%s" % node.__class__.__name__
-		method = getattr(self, methname, self.default)
-		return method(node, curScopeDict)
-    
-	def default(self, node):
-		'''Visit node children'''
-		print "Uh oh: A method doesn't exist for this node type: %s" % node.__class__.__name__
+from astvisitor import *
 
 class P2Uniquify(ASTVisitor):
 	# Private Attributes: #####################################################################################
